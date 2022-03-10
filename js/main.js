337704de35a0d8005712fd19f1e8215f729cb233
Eduardoduarte784 <eduardoduarte484@gmail.com>
2022-03-09 23:30:02 -0300

    Creating contact us page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 menuButton = document.getElementById('menu');
 menuButton.addEventListener('click', toggleMenu);
+adminEmail = 'admin123@example.com';
 
 function toggleMenu() {
   const nav = document.getElementById('nav');
   nav.classList.toggle('--active');
+}
+
+function sendContactMail() {
+  let email = document.querySelector("#email").value;
+  let body = document.querySelector("#body").value;
+  let subject = document.querySelector("#subject").value;
+
+  window.location.href = `mailto:${email}?body=${body}&subject=${subject}`;
+}
+
+function onClickSubmit(event) {
+  let password = document.querySelector("#password").value;
+  let confirmationPassword = document.querySelector("#passwordConfirmation").value;
+  
+  if (password !== confirmationPassword) {
+      event.preventDefault();
+      event.stopPropagation();
+      alert("A senha e a confirmação estão diferentes!");
+  }
 }
